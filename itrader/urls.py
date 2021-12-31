bfab2e2d1ace3cff3132ef4d98ade2acb6f0be88
Sharon-Kerubo <sharonkerubo0@gmail.com>
2021-12-31 13:01:11 +0300

    SIgnup and Login

--- itrader/urls.py
+++ itrader/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'itrade'
+app_name = 'itrader'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
 ]
--- itrader/views.py
+++ itrader/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     return render(request, "itrader/index.html")
+
+def login(request):
+    return render(request,  "itrader/login.html")
+
+def register(request):
+    # form = UserCreationForm()
+    # context = {'form': form}
+    return render(request,  "itrader/login.html") 
+    # context)
